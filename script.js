be3f70bf18c86921e32ff518061b35152fee3140
Kushal <kushalbhetuwal@gmail.com>
2023-04-26 22:17:32 +1000

    added button and js to the button

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const h2 = document.getElementsByTagName('h2')[0];
 h2.addEventListener('click', function(){
     h2.classList.toggle('toggle')
 })
+
+const button = document.querySelector('button')
+button.addEventListener("click", ()=>{
+    button.style.backgroundColor= "red";
+})
